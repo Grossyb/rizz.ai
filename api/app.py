6e2518825d072f8419bd75a41b54389c678cc400
Grossyb <grossyb@yahoo.com>
2023-04-25 15:46:42 -0700

    change Flask port

--- api/app.py
+++ api/app.py
@@ -132,4 +132,4 @@ def generate_responses():
     return jsonify(responses), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1',port=5000)
+    app.run(host='0.0.0.0', debug=True)
